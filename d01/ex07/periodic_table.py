bfee973d1499e469293aac2f8863e3ae64c1a276
Romain Wintgens <rwintgen@student.42madrid.com>
2025-03-12 22:00:24 +0100

    ex07 done-ish

--- d01/ex07/periodic_table.py
+++ d01/ex07/periodic_table.py
@@ -22,8 +22,68 @@ def parse_file():
 
 	return table
 
-# def create_html():
+def create_html(table):
+	with open("periodic_table.html", "w") as f:
+		f.write("""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Periodic Table</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+        td {
+            border: 1px solid black;
+            text-align: center;
+			width: 180px;
+			height: 180px;
+        }
+        ul {
+            padding: 0;
+            list-style-type: none;
+        }
+    </style>
+</head>
+<body>
+    <h1>Periodic Table of Elements</h1>
+	<table>
+		<tr>""")
+
+		current_pos = 0
+		for name, (position, number, symbol, mass, electrons) in table.items():
+			position = int(position)
+			if position == 0 & current_pos == 0:
+				current_pos = 0
+				f.write("""
+			</tr>
+			<tr>\n""")
+				
+			while current_pos < position:
+				f.write("""
+			<td></td>\n""")
+				current_pos += 1
+
+			f.write(f"""
+			<td style="border: 1px solid black; padding: 10px;">
+				<h4>{name}</h4>
+				<ul>
+					<li>No {number}</li>
+					<li>{symbol}</li>
+					<li>{mass}</li>
+					<li>{electrons} electrons</li>
+				</ul>
+			</td>\n""")
+			current_pos += 1
+		
+		f.write("""
+		</tr>
+	</table>
+</body>
+</html>""")
 
 if __name__ == '__main__':
 	table = parse_file()
-	# create_html(table)
+	create_html(table)
