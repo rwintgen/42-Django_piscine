fd27475c96031d30b59330655305e660b74ec024
Romain Wintgens <rwintgen@student.42madrid.com>
2025-03-15 13:50:31 +0100

    progressed ex06

--- d02/ex06/Page.py
+++ d02/ex06/Page.py
@@ -1,2 +1,26 @@
 from elements import Html, Head, Body, Title, Meta, Img, Table, Th, Tr, Td, Ul, Ol, Li, H1, H2, P, Div, Span, Hr, Br, Elem, Text
 
+class Page:
+	VALID_TAGS_FILE = {"html", "head", "body", "title", "meta", "img", "table", "th", "tr", "td", "ul", "ol", "li", "h1", "h2", "p", "div", "span", "hr", "br", "Text"}
+	VALID_TAGS_BODY = {"h1", "h2", "div", "table", "ul", "ol", "span", "Text"}
+	TEXT_ONLY_TAGS = {"title", "h1", "h2", "li", "th", "td"}
+
+	def __init__(self, elem):
+		if not isinstance(elem, Elem):
+			raise TypeError("Error: parameter must be of type Elem or any of its children.")
+		self.elem = elem
+
+	def __str__(self):
+		if self.elem.tag == "html":
+			source_code = "<!DOCTYPE html>\n"
+		source_code += str(self.elem)
+		return source_code
+	
+	# def is_valid(self):
+
+	def write_to_file(self, file_name):
+		try:
+			with open(file_name, "w") as f:
+				f.write(str(self.elem))
+		except Exception as e:
+			print(f"Error: {e}")
