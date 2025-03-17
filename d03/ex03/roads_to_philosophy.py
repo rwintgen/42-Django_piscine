598b8b62f349e58d3f55f3f7b19fd20086df7117
Romain Wintgens <rwintgen@student.42madrid.com>
2025-03-17 15:23:20 +0100

    ex03 done-ish

--- d03/ex03/roads_to_philosophy.py
+++ d03/ex03/roads_to_philosophy.py
@@ -2,37 +2,57 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-class WikiScraper:
-	BASE_URL = "https://en.wikipedia.org/wiki/"
-	GOAL_URL = BASE_URL + "Philosophy"
+def fetch_page(url):
+	response = requests.get(url)
+	response.raise_for_status()
+	return BeautifulSoup(response.text, "html.parser")
 
-	def __init__(self, start):
-		self.start_url = self.BASE_URL + start.replace(" ", "_")
-		self.visited = {start : self.start_url}
+def get_next_url(soup):
+	content = soup.find("div", {"id": "mw-content-text"})
 
-	def _get_first_link(self):
-		try:
-			response = requests.get(self.start_url)
-			response.raise_for_status()
-		except Exception as e:
-			if response.status_code == 404:
-				print("It's a dead end!")
-			else:
-				print("Error: " + str(e))
+	for link in content.select('p > a'):
+		href = link.get("href")
+		if href and href.startswith("/wiki/") and not href.startswith("/wiki/Wikipedia:") and not href.startswith("/wiki/Help:"):
+			new_link = "https://en.wikipedia.org" + href
+			print(new_link)
+			return new_link
+
+	return None
+
+def roads_to_philosophy(start):
+	url = "https://en.wikipedia.org" + "/wiki/" + start.replace(" ", "_")
+	visited = []
+
+	while url is not None:
+		soup = fetch_page(url)
+		title_tag = soup.find("h1", {"id": "firstHeading"})
+		title = title_tag.text if title_tag else "Unknown Title"
+		if title in visited:
+			print("It leads to an infinite loop!")
 			return
 
-		soup = BeautifulSoup(response.text, "html.parser")
-		# Continue here
+		visited.append(title)
+		print(title)
 
+		if title == "Philosophy":
+			print(f"{len(visited)} roads from {visited[0]} to Philosophy!")
+			return
+
+		url = get_next_url(soup)
+
+	print("It leads to a dead end!")
+
+def main():
+	if len(sys.argv) != 2:
+		print("Usage: python3 roads_to_philosophy.py <starting_page>")
+		return
+
+	try:
+		roads_to_philosophy(sys.argv[1])
+		# roads_to_philosophy("Txorierri line") # infinite loop
+	except Exception as e:
+		print(f"Error: {e}")
+		return
 
 if __name__ == '__main__':
-	if len(sys.argv) != 2:
-		print("Usage: python3 roads_to_philosophy.py <starting pqge>")
-		return
-	
-	try:
-		scrap = WikiScraper(sys.argv[1])
-		scrap.roads_to_philosophy()
-	except Exception as e:
-		print("Error: " + str(e))
-		return
+	main()
