720ccc274e892d68215e013f7471fa351be67ca9
Romain Wintgens <rwintgen@student.42madrid.com>
2025-03-17 12:16:54 +0100

    ex02 done-ish

--- d03/ex02/request_wikipedia.py
+++ d03/ex02/request_wikipedia.py
@@ -1,18 +1,42 @@
+import dewiki.parser
 import requests, json, dewiki, sys
+
+def create_url(query):
+	components = {
+		"base": "https://en.wikipedia.org/w/api.php",
+		"action": "parse",
+		"format": "json",
+		"page": query,
+		"prop": "wikitext",
+		"redirects": "true"
+	}
+
+	url = components["base"] + "?"
+	url += "&".join([f"{key}={value}" for key, value in components.items() if key != "base"])
+	# print(url)
+	return url
 
 def main():
 	if len(sys.argv) != 2:
 		print("Usage: python3 request_wikipedia.py <query>")
 		return
 	
-	query = sys.argv[1].replace(" ", "_")
-	response = requests.get(create_url(query))
-	# if repsonse not successful error
-	# get content from wiki page
-	# format content dewiki
-	# create file .wiki and write content
-	
+	try:
+		query = sys.argv[1].replace(" ", "_")
+		response = requests.get(create_url(query))
+		response.raise_for_status()
+		data = response.json()
+		# with open("raw_data.json", "w") as test:
+		# 	test.write(json.dumps(data, indent=4))
 
+		content = data["parse"]["wikitext"]["*"]
+		formatted_content = dewiki.from_string(content)
+		with open(f"{query}.wiki", "w") as out:
+			out.write(formatted_content)
 
-if __name__ == '__init__':
+	except Exception as e:
+		err_msg = str(e) if data["error"]["info"] is None else data["error"]["info"]
+		print("Error: " + err_msg)
+
+if __name__ == '__main__':
 	main()
