6cef39f7bedada4bf24fe49f6b156ba913ff9b7e
Romain Wintgens <rwintgen@student.42madrid.com>
2025-03-18 11:14:46 +0100

    project structure update

--- django_1.1_base_django/my_script.sh
+++ django_1.1_base_django/my_script.sh
@@ -3,4 +3,4 @@
 # Exec script using source cmd
 python3 -m venv django_venv
 source django_venv/bin/activate
-pip install --upgrade pip && pip install -r requirement.txt
+pip install --upgrade pip && pip install -r requirements.txt
