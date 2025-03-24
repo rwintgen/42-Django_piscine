7a695c42e3128a08617954aae848ede0afef31e9
Romain Wintgens <rwintgen@student.42madrid.com>
2025-03-24 15:16:51 +0100

    ex09 done

--- django_2.0_sql/orm/ex09/views.py
+++ django_2.0_sql/orm/ex09/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Planets, People
 
 # Create your views here.
+def display_content(request):
+	data = People.objects.select_related("homeworld").filter(
+		homeworld__climate__icontains="windy"
+	).order_by("name")
+
+	if not data:
+			return HttpResponse("No data available, please use the following command line before use: python3 manage.py loaddata ex09_initial_data.json")
+
+	context = {
+		"css_file": "ex09/styles.css",
+		"characters": data
+	}
+	return render(request, "ex09/display.html", context)
