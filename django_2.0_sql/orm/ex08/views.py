fd393edbd77e9d594f84729b0047f1e780228ccc
Romain Wintgens <rwintgen@student.42madrid.com>
2025-03-24 12:29:11 +0100

    ex08 done

--- django_2.0_sql/orm/ex08/views.py
+++ django_2.0_sql/orm/ex08/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
+from django.templatetags.static import static
+from django.http import HttpResponse, Http404
+from django.conf import settings
+import psycopg2
+import os
+import tempfile
 
 # Create your views here.
+def connect_to_db():
+	return psycopg2.connect(
+		dbname=settings.DATABASES['default']['NAME'],
+		user=settings.DATABASES['default']['USER'],
+		password=settings.DATABASES['default']['PASSWORD'],
+		host=settings.DATABASES['default']['HOST'],
+		port=settings.DATABASES['default']['PORT'],
+	)
+
+
+def rm_null_str_from_csv(file_path):
+	tmp = tempfile.NamedTemporaryFile(delete=False, mode='w', newline='')
+	with open(file_path, 'r') as f:
+		for line in f:
+			tmp.write(line.replace("NULL", "\\N"))
+	tmp.close()
+	return tmp.name
+
 def init_table(request):
 	try:
 		with connect_to_db() as conn:
@@ -39,6 +63,47 @@ def init_table(request):
 	except Exception as e:
 		return HttpResponse("Error: " + str(e), status = 500)
 
-def populate_table():
+def populate_table(request):
+	status = []
+	try:
+		with connect_to_db() as conn:
+			with conn.cursor() as cur:
+				planets_path = os.path.join(settings.BASE_DIR, "ex08", "static", "ex08", "planets.csv")
+				tmp = rm_null_str_from_csv(planets_path)
+				with open(tmp, 'r') as f:
+					cur.copy_from(f, "ex08_planets", "\t", columns=('name', 'climate', 'diameter', 'orbital_period', 'population', 'rotation_period', 'surface_water', 'terrain'))
+					status.append("OK")
 
-def display_content():
+				people_path = os.path.join(settings.BASE_DIR, "ex08", "static", "ex08", "people.csv")
+				tmp = rm_null_str_from_csv(people_path)
+				with open(tmp, 'r') as f:
+					cur.copy_from(f, "ex08_people", "\t", columns=('name', 'birth_year', 'gender', 'eye_color', 'hair_color', 'height', 'mass', 'homeworld'))
+					status.append("OK")
+
+			return HttpResponse("<br>".join(status))
+	except Exception as e:
+		return HttpResponse("Error: " + str(e), status = 500)
+
+def display_content(request):
+	try:
+		with connect_to_db() as conn:
+			with conn.cursor() as cur:
+				cur.execute("""
+					SELECT p.name AS character_name, p.homeworld, pl.climate
+					FROM ex08_people p
+					INNER JOIN ex08_planets pl ON p.homeworld = pl.name
+					WHERE pl.climate ILIKE '%windy%'
+					ORDER BY p.name;
+				""")
+				data = cur.fetchall()
+
+		if not data:
+			raise Http404()
+
+		context = {
+			"css_file" : "ex08/styles.css",
+			"characters" : data
+		}
+		return render(request, "ex08/display.html", context)
+	except Exception as e:
+		return HttpResponse("No data available")
