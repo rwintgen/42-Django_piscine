6b0684be427f5c44bdd01321b7bcc6e85dc2b819
rwintgen <rwintgen@student.42madrid.com>
2025-03-27 11:14:12 +0100

    revert: loginview not generic

--- django_3.1_advanced/advanced/ex/urls.py
+++ django_3.1_advanced/advanced/ex/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ArticleListView, HomeRedirectView, UserLoginView
+from .views import ArticleListView, HomeRedirectView, LoginView
 
 urlpatterns = [
 	path("", HomeRedirectView.as_view(), name="home"),
 	path("articles/", ArticleListView.as_view(), name="articles"),
-	path("login/", UserLoginView.as_view(), name="login"),
+	path("login/", LoginView.as_view(), name="login"),
 ]
--- django_3.1_advanced/advanced/ex/views.py
+++ django_3.1_advanced/advanced/ex/views.py
@@ -13,6 +13,15 @@ class ArticleListView(ListView):
 class HomeRedirectView(RedirectView):
 	url = reverse_lazy("articles")
 
-class UserLoginView(LoginView):
+class LoginView(FormView):
 	template_name = "login.html"
+	form_class = AuthenticationForm
 	success_url = reverse_lazy("home")
+
+	def form_valid(self, form):
+		user = form.get_user()
+		login(self.request, user)
+		return super().form_valid(form)
+
+	def form_invalid(self, form):
+		return self.render_to_response(self.get_context_data(form=form))
