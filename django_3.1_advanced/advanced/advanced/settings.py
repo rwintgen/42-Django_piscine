6229204ea293368660d219adea146a7ef26efcd3
rwintgen <rwintgen@student.42madrid.com>
2025-04-01 14:30:28 +0200

    ex05 finally done

--- django_3.1_advanced/advanced/advanced/settings.py
+++ django_3.1_advanced/advanced/advanced/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -112,6 +114,15 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+LANGUAGES = [
+	('en', _('English')),
+	('fr', _('French')),
+]
+
+LOCALE_PATHS = [
+	BASE_DIR / 'locale',
+]
 
 
 # Static files (CSS, JavaScript, Images)
